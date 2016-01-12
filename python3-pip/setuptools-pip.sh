65d6a34b51c46153117b859df92d10a20b22f2d9
Pas <pasthelod@gmail.com>
2016-01-12 03:53:40 +0100

    Use Python 3.5

--- python3-pip/setuptools-pip.sh
+++ python3-pip/setuptools-pip.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 
-echo 'deb http://archive.ubuntu.com/ubuntu/ vivid           main restricted universe multiverse' >  /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu/ vivid-backports main restricted universe multiverse' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu/ vivid-updates   main restricted universe multiverse' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu/ vivid-proposed  main restricted universe multiverse' >> /etc/apt/sources.list
-echo 'deb http://archive.ubuntu.com/ubuntu/ vivid-security  main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb http://archive.ubuntu.com/ubuntu/ xenial           main restricted universe multiverse' >  /etc/apt/sources.list
+echo 'deb http://archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb http://archive.ubuntu.com/ubuntu/ xenial-updates   main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb http://archive.ubuntu.com/ubuntu/ xenial-proposed  main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb http://archive.ubuntu.com/ubuntu/ xenial-security  main restricted universe multiverse' >> /etc/apt/sources.list
 
 
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 
-apt-get -y install python3.4 python3.4-dev wget zlib1g-dev libssl-dev libexpat1-dev libffi-dev pkg-config libreadline-dev libsqlite3-dev libbz2-dev libncursesw5-dev
+apt-get -y install python3.5 python3.5-dev wget zlib1g-dev libssl-dev libexpat1-dev libffi-dev pkg-config libreadline-dev libsqlite3-dev libbz2-dev libncursesw5-dev
 
 apt-get clean
 
-wget -q -O- https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python3.4
+wget -q -O- https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python3.5
 rm -f setuptools-*.*.*.tar.gz
 rm -f setuptools-*.zip
-wget -q -O- https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python3.4
+wget -q -O- https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python3.5
 
 ln -s /usr/bin/vim.tiny /usr/bin/vim
 
