d3225f0923e03eb38dd5977f81e8f5b805fc9fe4
Pas <pasthelod@gmail.com>
2016-12-05 12:30:50 +0100

    Prepare for externally mounted PostgreSQL data dir

--- maas/setup.sh
+++ maas/setup.sh
@@ -14,3 +14,19 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get -qq update
 apt-get install -y sudo maas
+
+systemctl stop postgresql
+
+mkdir -p /___init_data
+mv /var/lib/postgresql /___init_data/
+
+echo '
+[Service]
+After=local-fs.target
+Before=postgresql.service
+Type=oneshot
+ConditionPathExists=/___init_data/postgresql
+ExecStart=/bin/bash -c "mkdir -p /var/lib/postgresql ; mv /___init_data/postgresql/* /var/lib/postgresql/ ; chown -R postgres /var/lib/postgresql ; rmdir --ignore-fail-on-non-empty /___init_data"
+' > /etc/systemd/system/postgresql-prepare_db.service
+
+systemctl enable postgresql-prepare_db.service
