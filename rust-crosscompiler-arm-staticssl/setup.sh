a1e97c2feec0f9d148688b608d5f62b0e70a84d1
Juhász Sándor <blackghost1987@gmail.com>
2017-07-15 11:49:10 +0200

    try to crosscompile openssl with PIC support for static stuff

--- rust-crosscompiler-arm-staticssl/setup.sh
+++ rust-crosscompiler-arm-staticssl/setup.sh
@@ -17,14 +17,26 @@ cat >>/root/.cargo/config <<EOF
 linker = "arm-linux-gnueabihf-gcc"
 EOF
 
-# install ssl with PIC support for static linking
-wget -O /tmp/openssl.tgz https://www.openssl.org/source/openssl-1.0.2l.tar.gz && tar -zxf /tmp/openssl.tgz && cd openssl-1.0.2l && ./config -fPIC --prefix=/usr/local --openssldir=/usr/local/ssl && make && make install && cd ..
-
 export PATH=$PATH:/root/.cargo/bin
 rustup target add armv7-unknown-linux-gnueabihf
 
-echo '#!/bin/bash
-OPENSSL_LIB_DIR=/usr/local/lib/ OPENSSL_INCLUDE_DIR=/usr/local/include OPENSSL_STATIC=yes PKG_CONFIG_ALLOW_CROSS=1 PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig/ /root/.cargo/bin/cargo build --release $@ --target armv7-unknown-linux-gnueabihf' > /usr/local/bin/supercargo
+# install ssl with PIC support for static linking
+
+wget -O /tmp/openssl.tgz https://www.openssl.org/source/openssl-1.0.2l.tar.gz
+tar -zxf /tmp/openssl.tgz
+cd openssl-1.0.2l
+
+./config -fPIC --prefix=/root/opensslArm
+#--openssldir=/usr/local/ssl - is this needed?
+
+export cross=arm-linux-gnueabi-
+make CC="${cross}gcc" AR="${cross}ar r" RANLIB="${cross}ranlib"
+make install
+
+cd /root
+
+# echo '#!/bin/bash OPENSSL_LIB_DIR=/usr/local/lib/ OPENSSL_INCLUDE_DIR=/usr/local/include OPENSSL_STATIC=yes PKG_CONFIG_ALLOW_CROSS=1 PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig/ /root/.cargo/bin/cargo build --release $@ --target armv7-unknown-linux-gnueabihf' > /usr/local/bin/supercargo
+echo '#!/bin/bash OPENSSL_LIB_DIR=/root/opensslArm/lib/ OPENSSL_INCLUDE_DIR=/root/opensslArm/include OPENSSL_STATIC=yes PKG_CONFIG_ALLOW_CROSS=1 PKG_CONFIG_PATH=/usr/lib/arm-linux-gnueabihf/pkgconfig/ /root/.cargo/bin/cargo build --release $@ --target armv7-unknown-linux-gnueabihf' > /usr/local/bin/supercargo
 
 chmod +x /usr/local/bin/supercargo
 
