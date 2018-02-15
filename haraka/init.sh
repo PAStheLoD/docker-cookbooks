1bf72449af33ff6630e93b54b5750db3cf19743b
Pas <pasthelod@gmail.com>
2018-02-15 14:48:29 +0100

    plugin order matters (STARTTLS has to be advertised before auth)

--- haraka/init.sh
+++ haraka/init.sh
@@ -2,6 +2,22 @@
 
 cd /usr/local/haraka
 
+echo "
+access
+dnsbl
+helo.checks
+" > config/plugins
+
+if [[ ${tls_key} != "" ]] && [[ ${tls_cert} != "" ]] ; then
+    echo "tls" >> config/plugins
+
+    echo "
+    key=$tls_key
+    cert=$tls_cert
+    " > config/tls.ini
+    echo "Enabled TLS with key=$tls_key and cert=$tls_cert"
+fi
+
 if [[ ${relay_from_cidr} != "" ]] ; then
     for prefix in $(echo ${relay_from_cidr} | tr ',' ' ') ; do
         echo Relay enabled from: ${prefix}
@@ -34,16 +50,6 @@ else
     fi
 fi
 
-if [[ ${tls_key} != "" ]] && [[ ${tls_cert} != "" ]] ; then
-    echo "tls" >> config/plugins
-
-    echo "
-    key=$tls_key
-    cert=$tls_cert
-    " > config/tls.ini
-    echo "Enabled TLS with key=$tls_key and cert=$tls_cert"
-fi
-
 if [[ ${log_timestamps} != "" ]] ; then
     sed -i -r "s/timestamps=false/timestamps=true/" config/log.ini
 fi
@@ -55,6 +61,15 @@ fi
 if [[ ${debug} != "" ]] ; then
     sed -i -r "s/level=.*/level=debug/g" config/log.ini
 fi
+
+echo "
+mail_from.is_resolvable
+rcpt_to.in_host_list
+data.headers
+queue/smtp_forward
+max_unrecognized_commands
+" >> config/plugins
+
 
 exec haraka -c /usr/local/haraka 2>&1
 
