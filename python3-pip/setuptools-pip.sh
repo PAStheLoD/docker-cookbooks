4efdfed4742fd9f002bc51fc410b111db8855d50
Pas <pasthelod@gmail.com>
2018-11-15 16:55:17 +0100

    bump to py3.7

--- python3-pip/setuptools-pip.sh
+++ python3-pip/setuptools-pip.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 if [[ $(grep artful /etc/apt/sources.list | wc -l) = 0 ]] ; then
-    echo 'deb http://archive.ubuntu.com/ubuntu/ artful           main restricted universe multiverse' >>  /etc/apt/sources.list
-    echo 'deb http://archive.ubuntu.com/ubuntu/ artful-backports main restricted universe multiverse' >> /etc/apt/sources.list
-    echo 'deb http://archive.ubuntu.com/ubuntu/ artful-updates   main restricted universe multiverse' >> /etc/apt/sources.list
-    echo 'deb http://archive.ubuntu.com/ubuntu/ artful-proposed  main restricted universe multiverse' >> /etc/apt/sources.list
-    echo 'deb http://archive.ubuntu.com/ubuntu/ artful-security  main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb http://archive.ubuntu.com/ubuntu/ bionic           main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb http://archive.ubuntu.com/ubuntu/ bionic-backports main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb http://archive.ubuntu.com/ubuntu/ bionic-updates   main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb http://archive.ubuntu.com/ubuntu/ bionic-proposed  main restricted universe multiverse' >> /etc/apt/sources.list
+    echo 'deb http://archive.ubuntu.com/ubuntu/ bionic-security  main restricted universe multiverse' >> /etc/apt/sources.list
 fi
 
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 
-apt-get -y install python3.6 python3.6-dev wget zlib1g-dev libssl-dev libexpat1-dev libffi-dev pkg-config libreadline-dev libsqlite3-dev libbz2-dev libncursesw5-dev
+apt-get -y install python3.7 python3.7-dev wget zlib1g-dev libssl-dev libexpat1-dev libffi-dev pkg-config libreadline-dev libsqlite3-dev libbz2-dev libncursesw5-dev
 
 apt-get clean
 
-wget -q -O- https://bootstrap.pypa.io/get-pip.py | python3.6
+wget -q -O- https://bootstrap.pypa.io/get-pip.py | python3.7
 
 ln -s /usr/bin/vim.tiny /usr/bin/vim
 
