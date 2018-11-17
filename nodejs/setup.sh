6e0582e468ae9211017da00e633b62eb7bf27bfa
Pas <pas@zomg.hu>
2018-11-17 05:16:44 +0000

    nodejs fix

--- nodejs/setup.sh
+++ nodejs/setup.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -qq
 
-apt-get install -y wget ca-certificates apt-transport-https --no-install-recommends
+apt-get install -y wget ca-certificates apt-transport-https gnupg1 --no-install-recommends
 
 wget -q -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 
@@ -12,6 +12,8 @@ echo 'deb https://deb.nodesource.com/node_11.x cosmic main' > /etc/apt/sources.l
 
 apt-get update -qq
 apt-get install -y nodejs
+apt-get remove gnupg1 -y
+apt-get autoremove -y
 apt-get clean
 
 npm i -g yarn
