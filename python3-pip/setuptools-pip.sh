5a63363554041b7d215cf64fce2c93caf8292838
Pas <pas@coreconsult.hu>
2021-04-11 14:37:46 +0200

    python package bump

--- python3-pip/setuptools-pip.sh
+++ python3-pip/setuptools-pip.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [[ $(grep artful /etc/apt/sources.list | wc -l) = 0 ]] ; then
-    echo 'deb http://archive.ubuntu.com/ubuntu/  focal           main restricted universe multiverse' >  /etc/apt/sources.list
-    echo 'deb http://archive.ubuntu.com/ubuntu/  focal-backports main restricted universe multiverse' >> /etc/apt/sources.list
-    echo 'deb http://archive.ubuntu.com/ubuntu/  focal-updates   main restricted universe multiverse' >> /etc/apt/sources.list
-    echo 'deb http://archive.ubuntu.com/ubuntu/  focal-proposed  main restricted universe multiverse' >> /etc/apt/sources.list
-    echo 'deb http://security.ubuntu.com/ubuntu/ focal-security  main restricted universe multiverse' >> /etc/apt/sources.list
-fi
-
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
